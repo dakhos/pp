2d2717509f198cfcaf52a7e94bbcb33895b98a6d
Oleg <h1flyprod@gmail.com>
2019-02-08 13:52:41 +0600

    ya sdelal'

---
+++ PP2/Week1/Task4/Task4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string lineA = Console.ReadLine();//number of strings//
+            int a = int.Parse(lineA);//integer a accepts value from input//
+            for (int i = 1; i <= a; i++)
+            {
+                string s = new string('*', i);//number of * outputed in the line//
+                Console.WriteLine(s);
+            }
+            Console.ReadKey();
+        }
+    }
+}
