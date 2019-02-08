735592455b4fcc78c4de63409ee763b753042c4e
Oleg <h1flyprod@gmail.com>
2019-02-08 13:47:20 +0600

    task3

--- PP2/Week1/Task3/Task3/Program.cs
+++ PP2/Week1/Task3/Task3/Program.cs
@@ -6,7 +6,21 @@ namespace Task3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string line1 = Console.ReadLine();//first number input//
+            string line2 = Console.ReadLine();//second numbers input//
+            string[] parts = line2.Split(new char[] { ' ', ',', '$' });//split the second line//
+            int[] b = new int[200];//new array that will be outputed//
+            for (int i = 0; i < parts.Length; ++i)
+            {
+                int x = int.Parse(parts[i]);//x accepts the number from parts[i]//
+                b[i * 2] = x;//the next lines are doubling the new array - each 2 elements keep the same x//
+                b[i * 2 + 1] = x;
+            }
+            for (int i = 0; i < (parts.Length * 2); ++i)
+            {
+                Console.WriteLine(b[i]);
+            }
+            Console.ReadKey();
         }
     }
 }
