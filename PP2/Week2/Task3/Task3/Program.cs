7a55cfc1a18c13c90c1e2651fae85cf531476f75
Oleg <h1flyprod@gmail.com>
2019-02-08 17:19:41 +0600

    task3podsmotrelkapecslojna

---
+++ PP2/Week2/Task3/Task3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DirectoryInfo dir = new DirectoryInfo(@"C:\test");//information about folder//
+            PrintInfo(dir, 0);//printing the info//
+        }
+
+        static void PrintInfo(FileSystemInfo fsi, int k)
+        {
+            string s = new string(' ', k);
+            Console.WriteLine(s + fsi.Name);
+
+            if (fsi.GetType() == typeof(DirectoryInfo))
+            {
+                FileSystemInfo[] arr = ((DirectoryInfo)fsi).GetFileSystemInfos();
+                for (int i = 0; i < arr.Length; ++i)
+                {
+                    PrintInfo(arr[i], k + 3);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
