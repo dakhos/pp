852fc201e0ee13ced266a2a20f40ee2db87fae87
Oleg <h1flyprod@gmail.com>
2019-02-09 16:39:06 +0600

    vsetakisdelal

---
+++ PP2/Week2/Task4/Task4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pathString = @"c:\test";
+            string fileName = "MyNewFile.txt";
+            string targetPath = @"C:\test\test2";
+            string sourcePath = @"c:\test";
+            string sourceFile = System.IO.Path.Combine(sourcePath, fileName);
+            string destFile = System.IO.Path.Combine(targetPath, fileName);
+            pathString = System.IO.Path.Combine(pathString, fileName);
+            if (!System.IO.File.Exists(pathString))
+            {
+                using (System.IO.FileStream fs = System.IO.File.Create(pathString)) { }
+            }
+            else
+            {
+                Console.WriteLine("File \"{0}\" already exists.", fileName);
+                return;
+            }
+            System.IO.File.Copy(sourceFile, destFile, true);
+            System.IO.File.Delete(@"C:\test\MyNewFile.txt");
+        }
+    }
+}
