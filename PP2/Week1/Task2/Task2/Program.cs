e54a8fe98a4e572b6ca8a7fc2db30d88d70455b7
Oleg <h1flyprod@gmail.com>
2019-02-16 11:35:55 +0600

    upd

---
+++ PP2/Week1/Task2/Task2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Task2
+{
+    class Student
+    {
+        static void Main(string[] args)
+        {
+            int id = 1;
+            string[] arrn = new string[200];//to store info//
+            int[] arry = new int[200];
+            int n = Convert.ToInt32(Console.ReadLine());//number of students input//
+            for (int i = 0; i < n; ++i)
+            {
+                arrn[i] = Console.ReadLine();
+                int x = Convert.ToInt32(Console.ReadLine());
+                arry[i] = x;//inputting the data//
+            }
+            for (int i = 0; i < n; i++)//outputting the data//
+            {
+                Console.WriteLine("id " + id);
+                Console.WriteLine("name " + arrn[i]);
+                Console.WriteLine("year " + arry[i]);
+                id++;
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ PP2/Week3/Farmanager/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FarManager
+{
+    enum FARMode
+    {
+        DIR,
+        FILE
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FARMode mode = FARMode.DIR;
+            DirectoryInfo root = new DirectoryInfo(@"C:\");//basic directory//
+            Stack<Layer> history = new Stack<Layer>();
+            history.Push(
+                    new Layer
+                    {
+                        Content = root.GetFileSystemInfos().ToList(),
+                        SelectedItem = 0
+                    }
+                );
+
+
+            while (true)
+            {
+                if (mode == FARMode.DIR)
+                {
+                    history.Peek().Draw();
+                }
+                ConsoleKeyInfo consoleKeyInfo = Console.ReadKey();
+                switch (consoleKeyInfo.Key)
+                {
+                    case ConsoleKey.Delete://delete//
+                        history.Peek().DeleteSelectedItem();
+                        break;
+                    case ConsoleKey.UpArrow://up//
+                        history.Peek().SelectedItem--;
+                        break;
+                    case ConsoleKey.DownArrow://down//
+                        history.Peek().SelectedItem++;
+                        break;
+                    case ConsoleKey.Backspace://go back//
+                        if (mode == FARMode.DIR)
+                        {
+                            history.Pop();
+                        }
+                        else
+                        {
+                            mode = FARMode.DIR;
+                            Console.ForegroundColor = ConsoleColor.White;
+                        }
+                        break;
+                    case ConsoleKey.Enter://open dir//
+                        int x = history.Peek().SelectedItem;
+                        FileSystemInfo fileSystemInfo = history.Peek().Content[x];
+                        if (fileSystemInfo.GetType() == typeof(DirectoryInfo))
+                        {
+                            DirectoryInfo directoryInfo = fileSystemInfo as DirectoryInfo;
+                            history.Push(
+                               new Layer//executes another code Layer with new info about directory//
+                               {
+                                   Content = directoryInfo.GetFileSystemInfos().ToList(),
+                                   SelectedItem = 0
+                               });
+                        }
+                        else
+                        {
+                            mode = FARMode.FILE;
+                            Console.BackgroundColor = ConsoleColor.White;
+                            Console.Clear();
+                            Console.ForegroundColor = ConsoleColor.Black;
+                            using (StreamReader sr = new StreamReader(fileSystemInfo.FullName))
+                            {
+                                Console.WriteLine(sr.ReadToEnd());
+                            }
+                        }
+                        break;
+                }
+            }
+        }
+    }
+}
